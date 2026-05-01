0a6fbc8490cf26840c8a9025110895926b55c460
Rahul BK <rahulbc60@gmail.com>
2026-05-01 16:25:08 +0545

    fix wildcard route

--- backend/index.js
+++ backend/index.js
@@ -45,7 +45,7 @@ app.use("/api/otp", otpRoutes);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {   // 🔥 FIXED HERE
         res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
     });
 } else {
